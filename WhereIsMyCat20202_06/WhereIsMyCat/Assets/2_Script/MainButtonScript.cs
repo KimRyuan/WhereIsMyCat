49e131bbf9df3c5b5772c0a96c949189144404c9
KimRyuan <kdh990705@gmail.com>
2020-06-12 21:15:20 +0900

    아이조아~ 행복 유니티~~행복 깃허브~~
    히히히~ 고마워~~~ 인간들아~~~

---
+++ WhereIsMyCat20202_06/WhereIsMyCat/Assets/2_Script/MainButtonScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainButtonScript : MonoBehaviour
+{
+    public GameObject settingPanel;
+    public GameObject helpPanel;
+    public Canvas collectionCanvas;
+
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("ChapterSelectScene");
+    }
+
+    public void CollectionButtonClick()
+    {
+        collectionCanvas.enabled = true;
+    }
+
+    public void ExitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    public void SettingButtonClick()
+    {
+        settingPanel.SetActive(true);
+    }
+
+    public void HelpButtonClick()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    public void PanelBackButtonClick()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
