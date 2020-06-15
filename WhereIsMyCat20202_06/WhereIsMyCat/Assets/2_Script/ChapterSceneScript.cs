88155cb0514aa4afbd7612d8cfe05378a081a136
kimsohyeon67 <54832542+kimsohyeon67@users.noreply.github.com>
2020-06-15 21:19:07 +0900

    Merge For UI Data
    Merge For UI Data

--- WhereIsMyCat20202_06/WhereIsMyCat/Assets/2_Script/ChapterSceneScript.cs
+++ WhereIsMyCat20202_06/WhereIsMyCat/Assets/2_Script/ChapterSceneScript.cs
@@ -13,6 +13,10 @@ public class ChapterSceneScript : MonoBehaviour
     float[] pos;
     [SerializeField] int posisi = 0;
 
+
+
+    public GameObject[] ChapterStageBtn;
+
     public void ChapterNextButton()
     {
         if (posisi < pos.Length - 1)
@@ -20,7 +24,7 @@ public class ChapterSceneScript : MonoBehaviour
             posisi += 1;
             scroll_pos = pos[posisi];
         }
-       // ChapterImageCopy();
+        // ChapterImageCopy();
     }
 
     public void ChapterPrevButton()
@@ -31,28 +35,33 @@ public class ChapterSceneScript : MonoBehaviour
             scroll_pos = pos[posisi];
 
         }
-       // ChapterImageCopy();
+        // ChapterImageCopy();
     }
     private void Update()
     {
         pos = new float[transform.childCount];
         float distance = 1f / (pos.Length - 1f);
+
         for (int i = 0; i < pos.Length; i++)
 
         {
             pos[i] = distance * i;
+
         }
         if (Input.GetMouseButton(0))
         {
             scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
         }
+
         else
         {
             for (int i = 0; i < pos.Length; i++)
             {
                 if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
                 {
+
                     scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i], 0.15f);
+
                     posisi = i;
                     //ChapterImageCopy();
                 }
@@ -83,5 +92,38 @@ public class ChapterSceneScript : MonoBehaviour
     //    }
     //}
 
+    #region Chapter Iamge 아래 버튼 설정
+    public void StageButtonSetting()
+    {
+        int btnCount = 0;
 
+        switch (posisi)
+        {
+            case 0:
+                btnCount = 6;
+                break;
+            case 1:
+                btnCount = 4;
+                break;
+
+
+                //ChapterStageBtn
+        }
+
+        for (int i = 0; i < ChapterStageBtn.Length; i++)
+        {
+            if (i <= btnCount)
+                ChapterStageBtn[i].SetActive(true);
+            else
+                ChapterStageBtn[i].SetActive(false);
+        }
+
+
+    }
+    #endregion
+
+    #region 챕터 텍스트 이미지 설정
+
+
+    #endregion
 }
